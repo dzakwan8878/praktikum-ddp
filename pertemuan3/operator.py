1323aa1947f207f952cf4473961090cad3211c0d
dzakwan8878 <114457809+dzakwan8878@users.noreply.github.com>
2023-01-04 18:04:58 +0700

    Add files via upload

---
+++ pertemuan3/operator.py
@@ -0,0 +1,8 @@
+while True:
+    number = int(input("Masukkan Aangka:    "))
+    if number == 0:
+        break
+    if number % 2 == 0:
+        print("Bilangan Genap")
+    else:
+        print("Bilangan Ganjil")
